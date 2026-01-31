fca3acac90b3a08c90373f3a0b19fa07b3ff4f6f
Brindha P <brindhaarunkumar123@gmail.com>
2026-01-31 17:47:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function getQuote() {
-  fetch("https://api.quotable.io/random")
+  fetch("https://type.fit/api/quotes")
     .then(response => response.json())
     .then(data => {
+      const random = Math.floor(Math.random() * data.length);
       document.getElementById("quote").innerText =
-        data.content + " — " + data.author;
+        data[random].text + " — " + (data[random].author || "Unknown");
     })
     .catch(error => {
       document.getElementById("quote").innerText = "Error fetching quote";
